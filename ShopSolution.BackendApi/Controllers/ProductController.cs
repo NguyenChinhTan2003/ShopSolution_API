dc3a955cc415501305a6327b545d310d37a0e1c4
HP-TAN\nguye <nguyenchinhtan2003@gmail.com>
2024-11-05 16:20:16 +0700

    authorization header

--- ShopSolution.BackendApi/Controllers/ProductController.cs
+++ ShopSolution.BackendApi/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using ShopSolution.Application.Catalog.Products;
 
@@ -6,6 +7,7 @@ namespace ShopSolution.BackendApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class ProductController : ControllerBase
     {
         private readonly IPublicProductService _publicProductService;
--- ShopSolution.BackendApi/Program.cs
+++ ShopSolution.BackendApi/Program.cs
@@ -9,6 +9,9 @@ using Microsoft.AspNetCore.Identity;
 using ShopSolution.Application.System.Users;
 using ShopSolution.Application.System;
 using ShopSolution.Data.Entities;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -26,13 +29,81 @@ builder.Services.AddTransient<SignInManager<AppUser>, SignInManager<AppUser>>();
 builder.Services.AddTransient<RoleManager<AppRole>, RoleManager<AppRole>>();
 builder.Services.AddTransient<IUserService, UserService>();
 
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "Swagger Shop", Version = "v1" });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      \r\n\r\nExample: 'Bearer 12345abcdef'",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+            Reference = new OpenApiReference
+                {
+                Type = ReferenceType.SecurityScheme,
+                Id = "Bearer"
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header,
+            },
+            new List<string>()
+        }
+    });
 });
+
+string? issuer = builder.Configuration["Tokens:Issuer"];
+string? signingKey = builder.Configuration["Tokens:Key"];
+
+if (string.IsNullOrEmpty(issuer))
+{
+    throw new InvalidOperationException("JWT issuer configuration is missing");
+}
+
+if (string.IsNullOrEmpty(signingKey))
+{
+    throw new InvalidOperationException("JWT signing key configuration is missing");
+}
+
+byte[] signingKeyBytes = System.Text.Encoding.UTF8.GetBytes(signingKey);
+
+builder.Services.AddAuthentication(opt =>
+{
+    opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = false;
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuer = true,
+        ValidIssuer = issuer,
+        ValidateAudience = true,
+        ValidAudience = issuer,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ClockSkew = System.TimeSpan.Zero,
+        IssuerSigningKey = new SymmetricSecurityKey(signingKeyBytes)
+    };
+});
+        
+
+
 builder.Services.AddIdentity<AppUser, AppRole>()
                 .AddEntityFrameworkStores<ShopDBContext>()
                 .AddDefaultTokenProviders();
@@ -56,7 +127,11 @@ app.MapControllers();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+
+app.UseAuthentication();
+
 app.UseRouting();
+
 app.UseAuthorization();
 
 app.MapControllerRoute(
