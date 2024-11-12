c27cc4066ab06a99d7785edbdcf7b4c0cd55f409
SachHoang <145244407+SachHoang@users.noreply.github.com>
2024-11-12 20:57:37 +0700

    User list paging. DONE

---
+++ ShopSolution.Admin/Controllers/Components/PagerViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopSolution.ViewModels.Common;
+
+namespace ShopSolution.Admin.Controllers.Components
+{
+    public class PagerViewComponent : ViewComponent
+    {
+        public Task<IViewComponentResult> InvokeAsync(PagedResultBase result)
+        {
+            return Task.FromResult((IViewComponentResult)View("Default", result));
+        }
+    }
+}
