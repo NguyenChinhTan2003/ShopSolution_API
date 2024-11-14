c28b29e9711be82c586ee332a6cfc2cf930dccc7
HP-TAN\nguye <nguyenchinhtan2003@gmail.com>
2024-11-14 21:59:40 +0700

    register

--- ShopSolution.ViewModels/System/Users/RegisterRequest.cs
+++ ShopSolution.ViewModels/System/Users/RegisterRequest.cs
@@ -10,30 +10,41 @@ namespace ShopSolution.ViewModels.System.Users
     public class RegisterRequest
     {
         [Display(Name = "Tên")]
+        [Required(ErrorMessage = "Vui lòng nhập tên.")]
         public string? FirstName { get; set; }
 
         [Display(Name = "Họ")]
+        [Required(ErrorMessage = "Vui lòng nhập họ.")]
         public string? LastName { get; set; }
 
         [Display(Name = "Ngày sinh")]
         [DataType(DataType.Date)]
+        [Required(ErrorMessage = "Vui lòng nhập ngày sinh.")]
         public DateTime? Dob { get; set; }
 
         [Display(Name = "Email")]
+        [Required(ErrorMessage = "Vui lòng nhập email.")]
+        [EmailAddress(ErrorMessage = "Vui lòng nhập địa chỉ email hợp lệ.")]
         public string? Email { get; set; }
 
         [Display(Name = "Số điện thoại")]
+        [Phone(ErrorMessage = "Vui lòng nhập số điện thoại hợp lệ.")]
         public string? PhoneNumber { get; set; }
 
         [Display(Name = "Tài khoản")]
+        [Required(ErrorMessage = "Vui lòng nhập tài khoản.")]
         public string? UserName { get; set; }
 
         [Display(Name = "Mật khẩu")]
         [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Vui lòng nhập mật khẩu.")]
+        [StringLength(100, ErrorMessage = "Mật khẩu phải có ít nhất {2} ký tự.", MinimumLength = 6)]
         public string? Password { get; set; }
 
         [Display(Name = "Xác nhận mật khẩu")]
         [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Mật khẩu xác nhận không khớp.")]
         public string? ConfirmPassword { get; set; }
     }
+
 }
