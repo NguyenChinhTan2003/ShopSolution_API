369e92ac4ff0b1403635e7a04ddd31816822a73e
SachHoang <145244407+SachHoang@users.noreply.github.com>
2024-11-17 18:44:38 +0700

    Alert message using TempData. DONE

--- ShopSolution.Admin/Controllers/UserController.cs
+++ ShopSolution.Admin/Controllers/UserController.cs
@@ -32,6 +32,10 @@ namespace ShopSolution.Admin.Controllers
             };
             var data = await _userApiClient.GetUsersPagings(request);
             ViewBag.Keyword = keyword;
+            if (TempData["result"] != null)
+            {
+                ViewBag.SuccessMsg = TempData["result"];
+            }
             return View(data.ResultObj);
         }
 
@@ -56,8 +60,10 @@ namespace ShopSolution.Admin.Controllers
 
             var result = await _userApiClient.RegisterUser(request);
             if (result.IsSuccessed)
+            {
+                TempData["result"] = "Thêm mới người dùng thành công";
                 return RedirectToAction("Index");
-
+            }
             ModelState.AddModelError("", result.Message);
             return View(request);
         }
@@ -91,8 +97,10 @@ namespace ShopSolution.Admin.Controllers
 
             var result = await _userApiClient.UpdateUser(request.Id, request);
             if (result.IsSuccessed)
+            {
+                TempData["result"] = "Cập nhật người dùng thành công";
                 return RedirectToAction("Index");
-
+            }
             ModelState.AddModelError("", result.Message);
             return View(request);
         }
@@ -122,8 +130,10 @@ namespace ShopSolution.Admin.Controllers
 
             var result = await _userApiClient.Delete(request.Id);
             if (result.IsSuccessed)
+            {
+                TempData["result"] = "Xóa người dùng thành công";
                 return RedirectToAction("Index");
-
+            }
             ModelState.AddModelError("", result.Message);
             return View(request);
         }
