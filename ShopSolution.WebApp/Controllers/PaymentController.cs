dee86a156dbf1522ea403cbcbf2984afaf757595
HP-TAN\nguye <nguyenchinhtan2003@gmail.com>
2024-12-05 22:34:57 +0700

    Thanh toán VnPay

---
+++ ShopSolution.WebApp/Controllers/PaymentController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopSolution.ApiIntegration.VnPay;
+using ShopSolution.WebApp.Models.VnPay;
+
+namespace ShopSolution.WebApp.Controllers
+{
+    public class PaymentController : Controller
+    {
+
+        private readonly IVnPayService _vnPayService;
+        public PaymentController(IVnPayService vnPayService)
+        {
+
+            _vnPayService = vnPayService;
+        }
+
+        public IActionResult CreatePaymentUrlVnpay(PaymentInformationModel model)
+        {
+            var url = _vnPayService.CreatePaymentUrl(model, HttpContext);
+
+            return Redirect(url);
+        }
+
+        public IActionResult PaymentCallbackVnpay()
+        {
+            var response = _vnPayService.PaymentExecute(Request.Query);
+
+            return Json(response);
+        }
+
+
+    }
+
+}
---
+++ ShopSolution.WebApp/Service/IVnPayService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using ShopSolution.WebApp.Models.VnPay;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopSolution.ApiIntegration.VnPay
+{
+    public interface IVnPayService
+    {
+        string CreatePaymentUrl(PaymentInformationModel model, HttpContext context);
+        PaymentResponseModel PaymentExecute(IQueryCollection collections);
+
+    }
+}
