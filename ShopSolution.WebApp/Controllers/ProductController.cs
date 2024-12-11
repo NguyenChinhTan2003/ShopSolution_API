61021ac0a48ea99245dae16ad50b2d81b7ac8593
HP-TAN\nguye <nguyenchinhtan2003@gmail.com>
2024-12-12 02:31:22 +0700

    viewcount product

--- ShopSolution.WebApp/Controllers/ProductController.cs
+++ ShopSolution.WebApp/Controllers/ProductController.cs
@@ -1,6 +1,8 @@
 ﻿using LazZiya.ExpressLocalization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using ShopSolution.ApiIntegration;
+using ShopSolution.Data.EF;
 using ShopSolution.Utilities.Constants;
 using ShopSolution.WebApp.Models;
 using System.Globalization;
@@ -11,11 +13,12 @@ namespace ShopSolution.WebApp.Controllers
     public class ProductController : Controller
     {
         private readonly IProductApiClient _productApiClient;
+        private readonly ShopDBContext _shopDBContext;
 
-
-        public ProductController(IProductApiClient productApiClient)
+        public ProductController(IProductApiClient productApiClient,ShopDBContext shopDBContext)
         {
             _productApiClient = productApiClient;
+            _shopDBContext = shopDBContext;
 
         }
 
@@ -25,17 +28,25 @@ namespace ShopSolution.WebApp.Controllers
              ViewData["ShowSideComponent"] = false;
             // Gọi API để lấy thông tin sản phẩm
             var product = await _productApiClient.GetById(id, culture);
-
+            var products = await _shopDBContext.Products.FirstOrDefaultAsync(p => p.Id == id );
             if (product == null)
             {
                 return NotFound(); // Nếu không tìm thấy sản phẩm
             }
 
+            products.ViewCount++;
+            _shopDBContext.Products.Update(products);
+            await _shopDBContext.SaveChangesAsync();
+
             // Trả về View với dữ liệu chi tiết sản phẩm
             return View(new ProductDetailViewModel()
             {
-                Product = product
+                Product = product,
+
             });
+            
         }
+
+
     }
 }
