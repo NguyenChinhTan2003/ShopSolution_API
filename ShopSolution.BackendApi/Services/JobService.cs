c97da79e6a6fa7e340fe66a5621e60428e61ba5d
Tan <145244482+NguyenChinhTan2003@users.noreply.github.com>
2024-12-14 21:24:12 +0700

    Feature/count view product (#65)

---
+++ ShopSolution.BackendApi/Services/JobService.cs
@@ -0,0 +1,63 @@
+﻿using Hangfire;
+using Microsoft.EntityFrameworkCore;
+using ShopSolution.Application.Common;
+using ShopSolution.Data.EF;
+using ShopSolution.Utilities.Exceptions;
+using ShopSolution.ViewModels.Catalog.Products;
+
+namespace ShopSolution.BackendApi.Services
+{
+    public class JobService : IJobService
+    {
+        private readonly ShopDBContext _context;
+
+        public JobService(ShopDBContext context)
+        {
+            _context = context;
+        }
+
+        public void SendEmail()
+        {
+            Console.WriteLine($"Sending email to  with subject: at: {DateTime.Now}");
+        }
+
+        public async Task UpdateIsFeatured()
+        {
+            // Lấy danh sách sản phẩm cần đặt IsFeatured = true
+            var productsToFeature = await _context.Products
+                .Where(p => (p.Sold > 100 || p.ViewCount > 100) && p.IsFeatured != true)
+                .ToListAsync();
+
+            // Lấy danh sách sản phẩm cần đặt IsFeatured = false
+            var productsToUnfeature = await _context.Products
+                .Where(p => p.Sold <= 100 && p.IsFeatured == true)
+                .ToListAsync();
+
+            // Cập nhật trạng thái IsFeatured = true
+            if (productsToFeature.Any())
+            {
+                foreach (var product in productsToFeature)
+                {
+                    product.IsFeatured = true;
+                }
+            }
+
+            // Cập nhật trạng thái IsFeatured = false
+            if (productsToUnfeature.Any())
+            {
+                foreach (var product in productsToUnfeature)
+                {
+                    product.IsFeatured = false;
+                }
+            }
+
+            // Lưu thay đổi vào database nếu có cập nhật
+            if (productsToFeature.Any() || productsToUnfeature.Any())
+            {
+                await _context.SaveChangesAsync();
+            }
+        }
+
+
+    }
+}
