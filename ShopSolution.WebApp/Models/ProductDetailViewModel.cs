cd9213ddee1e68f88aa3fab0e99d93cbb47bb4de
HP-TAN\nguye <nguyenchinhtan2003@gmail.com>
2024-12-15 02:03:20 +0700

    add product on productDetail + fix Hangfire

--- ShopSolution.WebApp/Models/ProductDetailViewModel.cs
+++ ShopSolution.WebApp/Models/ProductDetailViewModel.cs
@@ -13,5 +13,7 @@ namespace ShopSolution.WebApp.Models
         public List<ProductVm>? RelatedProducts { get; set; }
 
         public List<ProductImageViewModel>? ProductImages { get; set; }
+        public List<ProductVm>? FeaturedProducts { get; set; }
+
     }
 }
